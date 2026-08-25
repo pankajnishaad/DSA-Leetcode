cf5e0075120a88293c9761fe581cdf08edfe5e52
Pankaj Nishad <144531286+pankajnishaad@users.noreply.github.com>
2026-08-25 18:34:11 +0530

    Time: 1 ms (98.03%), Space: 42.4 MB (80.46%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,15 +1,21 @@
 class Solution {
     public int addDigits(int num) {
-        while(num>=10)
-        {
-            int sum=0;
-            while(num>0){
-            int dig=num%10;
-            sum+=dig;
-            num/=10;
-            }
-            num=sum;
+        // while(num>=10)
+        // {
+        //     int sum=0;
+        //     while(num>0){
+        //     int dig=num%10;
+        //     sum+=dig;
+        //     num/=10;
+        //     }
+        //     num=sum;
+        // }
+        // return num;
+
+        if (num==0) {
+            return 0;
         }
-        return num;
+        // Digital root formula
+        return 1+(num-1)%9;
     }
 }
