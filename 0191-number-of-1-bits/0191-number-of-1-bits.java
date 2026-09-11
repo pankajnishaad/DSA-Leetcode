f30c64f93c461ff330eac256dc25e85673714da6
Pankaj Nishad <144531286+pankajnishaad@users.noreply.github.com>
2026-09-11 10:37:29 +0530

    Time: 1 ms (29.35%), Space: 42.4 MB (46.81%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -3,8 +3,11 @@ class Solution {
         int count=0;
         while(n!=0)
         {
-            n=n&(n-1);
-            count++;
+            if((n&1)==1)
+            {
+                count++;
+            }
+            n=n>>1;
         }
         return count;
     }
